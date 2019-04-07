0cd0581a6827b92dbc2cc5f197d7229618b273d1
Tshepang <1431795@students.wits.ac.za>
2019-04-07 21:52:13 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -12,21 +12,21 @@ if __name__ == '__main__':
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
 
-    try:
-        import django
+        try:
+            import django
         #from django.core.management import execute_from_command_line
    # except ImportError as exc:
    #        "Couldn't import Django. Are you sure it's installed and "
     #        "available on your PYTHONPATH environment variable? Did you "
     #        "forget to activate a virtual environment?"
     
-    except ImportError:
+        except ImportError:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
             )
 
-        )# from exc
+        #)# from exc
         raise
     execute_from_command_line(sys.argv)

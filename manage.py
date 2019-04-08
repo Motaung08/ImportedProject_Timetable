8ecdee5424280dfc35989c13190e4a202c4efd4f
Tshepang <1431795@students.wits.ac.za>
2019-04-08 06:37:42 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         try:
             import django
         #from django.core.management import execute_from_command_line
-   # except ImportError as exc:
+        # except ImportError as exc:
    #        "Couldn't import Django. Are you sure it's installed and "
     #        "available on your PYTHONPATH environment variable? Did you "
     #        "forget to activate a virtual environment?"
